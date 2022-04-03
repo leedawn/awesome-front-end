cdc5079aa25ad1333f8f77112f66d1b47003ff59
leon <1558449520@qq.com>
2022-04-03 15:36:56 +0800

    add mini-vuex

---
+++ awesome-vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ count }} *2 = {{ double }}</div>
+  <button @click="add">add</button>
+  <button @click="asyncAdd">async add</button>
+  {{ name }}
+</template>
+
+<script setup>
+import { computed, inject } from "vue";
+import { useStore } from "./store/gvuex";
+let store = useStore();
+let count = computed(() => store.state.count);
+let double = computed(() => store.getters.double);
+let name = inject("name");
+
+function asyncAdd() {
+  const result = store.dispatch("asyncAdd");
+  console.log(result);
+}
+function add() {
+  store.commit("add");
+}
+</script>
